67d787e70a6858844c4309649f94dd2c04ae553a
k-logic24 <49090551+k-logic24@users.noreply.github.com>
2022-03-10 22:04:37 +0900

    修正

--- src/views/auth/Entry.vue
+++ src/views/auth/Entry.vue
@@ -6,8 +6,8 @@
         id="email"
         type="email"
         class="form-control"
-        required
         v-model="email"
+        required
       />
     </div>
     <input type="submit" class="btn btn-primary" value="送信する" />
@@ -29,19 +29,17 @@ export default defineComponent({
   },
   methods: {
     signin() {
-      if (this.email) {
-        api.auth
-          .signInLinkToEmail(this.email)
-          .then(() => {
-            window.localStorage.setItem("emailForSignIn", this.email);
-            this.$router.push("/confirm");
-          })
-          .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(`${errorCode}: ${errorMessage}`);
-          });
-      }
+      api.auth
+        .signInLinkToEmail(this.email)
+        .then(() => {
+          window.localStorage.setItem("emailForSignIn", this.email);
+          this.$router.push("/confirm");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(`${errorCode}: ${errorMessage}`);
+        });
     },
   },
 });
